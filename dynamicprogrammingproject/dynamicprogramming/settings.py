347082fe4fa542874ca93cc7bfc822112fe724d2
Jguerra47 <jguerra47canada@gmail.com>
2022-04-07 11:19:41 -0500

    allowed host for GCP

--- dynamicprogrammingproject/dynamicprogramming/settings.py
+++ dynamicprogrammingproject/dynamicprogramming/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6^=@zcml*sgvell+tal6)7(3*0rry0_l^$74+__nq4q1)v^i1w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.66.95.149']
 
 
 # Application definition
