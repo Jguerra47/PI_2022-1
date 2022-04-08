cd2da2e7ebb1eedf5be662a883355fcba3a4eada
Jguerra47 <jguerra47canada@gmail.com>
2022-04-07 22:04:03 -0500

    home update

--- dynamicprogrammingproject/dp/views.py
+++ dynamicprogrammingproject/dp/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    #return HttpResponse('<h1>Welcome to Home Page</h1>')
-    return render(request, 'home.html', {'name':'Sebastian'})
+from .models import Movie
 
-# Create your views here.
+def home(request):
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request,'home.html', {'searchTerm':searchTerm,'movies':movies})
--- dynamicprogrammingproject/dynamicprogramming/settings.py
+++ dynamicprogrammingproject/dynamicprogramming/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6^=@zcml*sgvell+tal6)7(3*0rry0_l^$74+__nq4q1)v^i1w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.193.171.68']
+ALLOWED_HOSTS = ['35.193.171.68', '127.0.0.1']
 
 # Application definition
 
